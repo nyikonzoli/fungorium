34a9465f2102578c063fee088f5633313d1d5739
Venter Bálint <vbalint1414@gmail.com>
2025-03-20 23:28:19 +0100

    Menu added

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Menu menu = new Menu();
+        menu.chooseOption();
     }
 }
---
+++ src/Menu.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class Menu {
+    public void displayMenu() {
+        System.out.print("""
+                
+                1: Start new round
+                2: Grow mycelium to Tekton
+                3: Grow mycelium to Infertile Tekton
+                4: Grow mycelium to Absorbing Tekton
+                5: Grow mycelium to Deadend Tekton
+                6: Cut mycelium
+                7: Eat normal spore
+                8: Eat slowing spore
+                9: Eat speeding spore
+                10: Eat paralyzing spore
+                11: Eat cut blocking spore
+                12: Move insect
+                13: Tekton split
+                14: Grow mushroom
+                15: Grow super mushroom
+                16: Spread spores as super mushroom
+                0: Exit
+                """);
+    }
+
+    public void chooseOption() {
+        Scanner sc = new Scanner(System.in);
+        int choice;
+
+        do {
+            displayMenu();
+            System.out.print("Enter your choice: ");
+
+            while (!sc.hasNextInt()) {
+                System.out.println("Invalid input");
+                sc.next();
+            }
+
+            choice = sc.nextInt();
+
+            if(choice < 0 || choice > 16){
+                System.out.println("Invalid input");
+            }
+
+        }
+        while (choice != 0);
+
+        System.out.println("\n");
+
+        sc.close();
+    }
+}
