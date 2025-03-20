0142e2eb2d3cc229bb0d80bb3af38882b3927a9a
Nyikon Zoltán Ferenc <nyikon.zoli@gmail.com>
2025-03-20 12:18:59 +0100

    Create Player.java

---
+++ src/Player.java
@@ -0,0 +1,19 @@
+public class Player {
+    private int score;
+
+    public Player(){}
+
+    public int getScore(){
+        return score;
+    }
+
+    public void setScore(int score){
+        this.score = score;
+    }
+
+    public void onRoundStart(){}
+
+    public void selfReport(){}
+
+    public void incrementScore(){}
+}
