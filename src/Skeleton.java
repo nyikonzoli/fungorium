16db0953dbf6983f51eaeac86d73f49747c35dc0
Nyikon Zoltán Ferenc <nyikon.zoli@gmail.com>
2025-03-21 21:40:43 +0100

    Create Skeleton.java

---
+++ src/Skeleton.java
@@ -0,0 +1,31 @@
+
+import java.util.HashMap;
+
+public class Skeleton {
+    HashMap<String, Object> objects;
+
+    public Skeleton(){
+        objects = new HashMap<>();
+
+        // objects.put("tN", null);
+        // objects.put("tN2", null);
+        // objects.put("mmaster", null);
+        // objects.put("tH", null);
+        // objects.put("tI", null);
+        // objects.put("tD", null);
+        // objects.put("tA", null);
+        // objects.put("insect", null);
+        // objects.put("imaster", null);
+        // objects.put("regularSpore", null);
+        // objects.put("speedingSpore", null);
+        // objects.put("slowingSpore", null);
+        // objects.put("paralyzingSpore", null);
+        // objects.put("cbSpore", null);
+        // objects.put("game", null);  gameField és players mezőket feltölteni
+
+    }
+
+
+
+
+}
