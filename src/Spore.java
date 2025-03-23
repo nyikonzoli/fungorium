37cf47d414c1a52a8f75ed6a95d26270357449e5
Ertsey Kristóf <kristof.ertsey@gmail.com>
2025-03-23 18:47:56 +0100

    Readd spore javadoc

--- src/Spore.java
+++ src/Spore.java
@@ -1,3 +1,7 @@
 public abstract class Spore {
+    /**
+     * Applies an effect on an insect
+     * @param i Insect subject to the applied effect
+     */
     public abstract void applyEffect(Insect i);
 }
