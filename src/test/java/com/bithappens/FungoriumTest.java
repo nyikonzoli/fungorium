5860fa9612c2ed4b99e858cc9dcd9616034b31f6
Kristóf Ertsey <kristof.ertsey@gmail.com>
2025-04-24 14:45:30 +0200

    Added JUnit5 and FungoriumTest class

---
+++ src/test/java/com/bithappens/FungoriumTest.java
@@ -0,0 +1,13 @@
+package com.bithappens;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class FungoriumTest {
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+}
